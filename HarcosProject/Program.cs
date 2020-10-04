2c881f86d1a284526f2264c6df5ee945cc189156
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-10-04 21:20:19 +0200

    2a

--- HarcosProject/Program.cs
+++ HarcosProject/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,32 @@ namespace HarcosProject
 {
     class Program
     {
+
+        static List<Harcos> harcosok = new List<Harcos>();
+
         static void Main(string[] args)
         {
+
+
+
         }
+
+        public static void beOlvas()
+        {
+            harcosok = new List<Harcos>();
+            
+            StreamReader sr = new StreamReader("harcosok.csv");
+            while (!sr.EndOfStream)
+            { 
+            
+            
+            
+            }
+        
+        
+        
+        
+        }
+
     }
 }
