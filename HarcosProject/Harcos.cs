1bc5a92cec3e99f89eea0222f2f80dccfa5b291c
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-10-04 20:09:31 +0200

    UML táblázat done

--- HarcosProject/Harcos.cs
+++ HarcosProject/Harcos.cs
@@ -8,5 +8,40 @@ namespace HarcosProject
 {
     class Harcos
     {
+        private string nev;
+        private int szint;
+        private int tapasztalat;
+        private int eletero;
+        private int alapEletero;
+        private int alapSzebzés;
+
+        public Harcos(string nev, int statuszSablon)
+            { 
+
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Szint { get => szint; set => szint = value; }
+        public int Tapasztalat { get => tapasztalat; set => tapasztalat = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int AlapEletero { get => alapEletero;  }
+        public int AlapSzebzés { get => alapSzebzés;  }
+        public int SzintLepeshez { get => ;  }
+        public int MaxEletero { get => ;  }
+
+
+        public void MegKuzd(Harcos masikharcos) 
+        {
+        }
+        public void Gyogyul()
+        { 
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+
+
     }
 }
