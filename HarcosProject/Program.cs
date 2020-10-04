01088c5b2a79f1d7cc73025989fef83eefa7e1e2
Kiss Péter Attila <kisspepe2002@gmail.com>
2020-10-04 21:30:31 +0200

    2 kész

--- HarcosProject/Program.cs
+++ HarcosProject/Program.cs
@@ -14,7 +14,7 @@ namespace HarcosProject
 
         static void Main(string[] args)
         {
-
+            beOlvas();
 
 
         }
@@ -22,17 +22,31 @@ namespace HarcosProject
         public static void beOlvas()
         {
             harcosok = new List<Harcos>();
-            
+           
             StreamReader sr = new StreamReader("harcosok.csv");
             while (!sr.EndOfStream)
-            { 
-            
+            {
+                string sor = sr.ReadLine();
+                string[] bontottSor = sor.Split(';');
+                Harcos harcosBe = new Harcos(bontottSor[0], Convert.ToInt32(bontottSor[1]));
+                harcosok.Add(harcosBe);
             
             
             }
-        
-        
-        
+            sr.Close();
+            harcosok.Add(new Harcos("Romeo", 1));
+            harcosok.Add(new Harcos("Julia", 2));
+            harcosok.Add(new Harcos("Albert", 3));
+            
+            
+
+            Console.WriteLine("A játék harcosai:\n");
+
+            for (int i = 0; i < harcosok.Count; i++)
+            {
+                Console.WriteLine(harcosok[i]);
+            }
+            Console.ReadKey();
         
         }
 
